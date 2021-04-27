f2696ce82209fd845e7895812a05baf816db8f97
Alif Bhadrika Parikesit <alifbhadrika@gmail.com>
2021-04-27 17:37:50 +0700

    find all date in query

--- src/backend.py
+++ src/backend.py
@@ -1,5 +1,4 @@
 import re
-import itertools
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from datetime import datetime
@@ -11,16 +10,36 @@ keywords = {'tubes','quiz','tucil','tubes','pr'}
 months_synonym = {'agu' : 'aug', 'mei' : 'may', 'des' : 'dec', 'okt' : 'oct'}
 
 
-kalimat = 'Halo bot, tolong ingetin aku ya ada tubes if2211 bab 2 sampai 3 pada 14-10-2012'
+kalimat = 'Halo bot, tolong ingetin aku 3 hari ya ada pada 14-10-2012 sampai 13 oktober 2021 tubes if2211 bab 2 sampai 3 cacatt loo'
 
-tgl = re.search(r'(\d{2})[/-](\d{2})[/-](\d{4})',kalimat)
-tgl_bulan = re.search(r'(\b\d{1,2}\D{0,3})?\b(?:[jJ]an(?:uari)?|[fF]eb(?:ruari)?|[mA]ar(?:et)?|[aA]pr(?:il)?|[mM]ei|[jJ]un(?:i)?|[jJ]ul(?:i)?|[aA]ug(?:ust)?|[aA]gustus?|[sS]ep(?:tember)?|[oO][ck]t(?:ober)?|([nN]ov|[dD]e[cs])(?:ember)?)\D?(\d{1,2}\D?)?\D?((19[7-9]\d|20\d{2})|\d{2})',kalimat)
-matkul = re.search(r'([kK][uU]|[iI][fF])(\d{2})(\d{2})',kalimat)
+date1pattern = r'(\d{2})[/-](\d{2})[/-](\d{4})'
+date2pattern = r'(\b\d{1,2}\D{0,3})?\b(?:[jJ]an(?:uari)?|[fF]eb(?:ruari)?|[mA]ar(?:et)?|[aA]pr(?:il)?|[mM]ei|[jJ]un(?:i)?|[jJ]ul(?:i)?|[aA]ug(?:ust)?|[aA]gustus?|[sS]ep(?:tember)?|[oO][ck]t(?:ober)?|([nN]ov|[dD]e[cs])(?:ember)?)\D?(\d{1,2}\D?)?\D?((19[7-9]\d|20\d{2})|\d{2})'
+kodepattern = r'([kK][uU]|[iI][fF])(\d{2})(\d{2})'
+timeperiodpattern = r'\b[0-9]+?\b\s([hH]ari|[mM]inggu)'
+
+
+def timeperiod(S):
+    time = re.search(timeperiodpattern,S)
+    if time:
+        return time.group()
+    
+
+print(timeperiod(kalimat))
+
+def findalldate(S):
+    date = []
+    date1 = re.search(date1pattern,S)
+    date2 = re.search(date2pattern,S)
+    date.append(str(date1.group()))
+    date.append(str(date2.group()))
+    return date
+
+print(findalldate(kalimat))
 
 def date(S):
-    date = re.search(r'(\d{2})[/.-](\d{2})[/.-](\d{4})',S)
+    date = re.search(date1pattern,S)
     if not date:
-        date = re.search(r'(\b\d{1,2}\D{0,3})?\b(?:[jJ]an(?:uari)?|[fF]eb(?:ruari)?|[mA]ar(?:et)?|[aA]pr(?:il)?|[mM]ei|[jJ]un(?:i)?|[jJ]ul(?:i)?|[aA]ug(?:ust)?|[aA]gustus?|[sS]ep(?:tember)?|[oO][ck]t(?:ober)?|([nN]ov|[dD]e[cs])(?:ember)?)\D?(\d{1,2}\D?)?\D?((19[7-9]\d|20\d{2})|\d{2})',S)
+        date = re.search(date2pattern,S)
         if (not date):
             return
         datelist = date.group().split(' ')
@@ -39,6 +58,20 @@ def date(S):
 
 print(date(kalimat).date())
 
+def matkul(S):
+    kode = re.search(kodepattern,S)
+    if (kode):
+        return kode.group().upper()
+    return
+
+print(matkul(kalimat))
+
+def spek(S):
+    kode = re.search(kodepattern,S)
+    if (kode):
+        return S.partition(kode.group()+str(' '))[2]
+
+print(spek(kalimat))
 
 # def toRegex(S):
 #     S = stemmer.stem(stopword.remove(S))
