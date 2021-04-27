c9faf269f1284b7a9a0f457195944eb6d502cc08
ayahyaaa <68509074+ayahyaaa@users.noreply.github.com>
2021-04-27 14:31:20 +0700

    add bm according to slide
    make dictionary, return index huruf pertama yg match sama pattern, return -1 kalo no match

---
+++ src/bm.py
@@ -0,0 +1,45 @@
+def buildLastArray(stringInput):
+    last = {}
+    
+    count = 0
+    for char in stringInput:
+        last[char] = count
+        count += 1
+
+    return last
+
+def bm(text,stringInput):
+    last = buildLastArray(stringInput)
+    n = len(text)
+    m = len(stringInput)
+    i = m-1
+    j = m-1
+
+    if (i>n-1):
+        print("pattern's longer than text, no match")
+        return -1
+    
+    while (i<=n-1):
+        if (stringInput[j] == text[i]):
+            if (j==0):
+                return i
+            else:
+                i -= 1
+                j -=1
+        else:
+            lastOccurence = 0
+            if text[i] in last:
+                lastOccurence = last.get(text[i])
+            else:
+                lastOccurence = -1
+            i = i + m - min(j,1+lastOccurence)
+            j = m-1
+
+    return -1
+
+'''
+text = "akuseringpergikepasaruntukbeliayamgorengdepanrumah"
+pat = "ayamgoreng"
+hasil = bm(text,pat)
+print(hasil)
+'''
